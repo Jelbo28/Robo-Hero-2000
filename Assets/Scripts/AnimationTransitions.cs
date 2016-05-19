fcf94af77fcc591bac63f318e3243d12c3e1ca12
Aram Joseph <ajoseph@utc4me.org>
2016-05-19 17:03:04 -0400

    Good

---
+++ Assets/Scripts/AnimationTransitions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AnimationTransitions : MonoBehaviour
+{
+    [SerializeField]
+    Animator exitAnimator;
+
+    public void Menu()
+    {
+        Debug.Log("Back to menu.");
+        // Use this to load whatever scene the title screen is on.
+        //SceneManager.LoadScene("Title Menu");
+    }
+
+    public void ButtonGlow()
+    {
+        exitAnimator.SetBool("ButtonGlow", true);
+    }
+}
